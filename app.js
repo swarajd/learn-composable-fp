1ad7ac9aaff3b8f4dad06730653474ae00c12595
Swaraj Dhumne <swaraj.dhumne@gmail.com>
2016-12-20 12:04:23 -0500

    added app for debug

---
+++ app.js
@@ -0,0 +1,31 @@
+const {Right, Left} = require('./Either')
+
+const fromNullable = x =>
+    x != null ? Right(x) : Left(null)
+
+const fs = require('fs')
+
+const tryCatch = f => {
+    try {
+        return Right(f())
+    } catch(e) {
+        console.log(e);
+        return Left(e)
+    }
+}
+
+const readFile = x => tryCatch(() => fs.readFileSync(x, "utf-8"))
+
+const wrapExample = example =>
+    fromNullable(example.previewPath)
+    .chain(readFile)
+    .fold(() => example,
+          p => Object.assign({preview: p}, example))
+
+let exmp = {
+    previewPath: "./preview.txt"
+}
+
+let result = wrapExample(exmp);
+
+console.log(result);    
