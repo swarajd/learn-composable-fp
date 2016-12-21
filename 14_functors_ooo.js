638ae68e2ccb68b513bd7f04432c409402b7bc52
Swaraj Dhumne <swaraj.dhumne@gmail.com>
2016-12-20 23:57:18 -0500

    finished 14

--- 14_functors_ooo.js
+++ 14_functors_ooo.js
@@ -3,3 +3,22 @@ const Task = require("data.task")
 const Either = require("./Either")
 const {Right, Left, fromNullable} = Either
 const {List, Map} = require("immutable-ext")
+
+//fx.map(f).map(g) === fx.map(x => f(g(x)))
+//res1 === res2
+// const res1 = Box("squirrels")
+//             .map(s => s.substr(5))
+//             .map(s => s.toUpperCase())
+
+// const res2 = Box("squirrels")
+//             .map(s => s.substr(5).toUpperCase())
+
+
+
+//fx.map(id) === id(fx)
+//res1 === res2
+const id = x => x
+const res1 = List.of("crayons").map(id)
+const res2 = id(List.of("crayons"))
+
+console.log(res1, res2)
