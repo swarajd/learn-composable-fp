0eb0d72c48c3368bcb6efa973e603330a67fcd5c
Swaraj Dhumne <swaraj.dhumne@gmail.com>
2016-12-20 21:52:11 -0500

    finished 12

---
+++ 12_sideeffects_in_task.js
@@ -0,0 +1,14 @@
+const Task = require("data.task")
+
+const launchMissiles = () =>
+    new Task((rej, res) => {
+        console.log("launch missiles!")
+        res("missile")
+    })
+    
+
+const app = launchMissiles().map(x => x + "!")
+
+
+app.map(x => x + "!").fork(e => console.error("err", e),
+                           x => console.log("success", x))
