5dc0c770e22163eb832124a62e4238586d3ad78b
Swaraj Dhumne <swaraj.dhumne@gmail.com>
2016-12-21 19:26:58 -0500

    have more of the data flow done

--- 27.js
+++ 27.js
@@ -1,19 +1,18 @@
-// "https://api.spotify.com/v1/search?q=${query}&type=artist" //artists: {items: []}
-// "https://api.spotify.com/v1/artists/${id}/related-artists" //artists: []
-
 const Task = require("data.task")
+const Spotify = require("./Spotify")
 
 const argv = new Task((rej, res) => res(process.argv))
 const names = argv.map(args => args.slice(2))
 
 const related = name => 
-    findArtist(name1)
+    Spotify.findArtist(name)
     .map(artist => artist.id)
-    .chain(relatedArtists)
+    .chain(Spotify.relatedArtists)
+    .map(artists => artists.map(artist => artist.name))
 
 const main = ([name1, name2]) =>
     Task.of(rels1 => rels2 => [rels1, rels2])
     .ap(related(name1))
     .ap(related(name2))
 
-names.map(main).fork(console.error, console.log)
+names.chain(main).fork(console.error, console.log)
